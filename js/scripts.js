951ae97306be330a7840f9e774c92d8ea9ab7f3c
memetibryan <memetibryan@gmail.com>
2017-05-04 15:11:40 +0300

    first specification'show any result'achieved

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-jQuery("img").click(function() {
-  alert("This is an image.");
+/*user interface logic*/
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+      event.preventDefault();
+      var numbe = parseInt($("input#numbe").val());
+      var result = pingPong(numbe);
+      $("#result").text(result);
+    });
 });
+
+/*bussiness logic*/
+var pingPong = function(numbe) {
+    if ((numbe % 4 === 0) && (numbe % 100 !== 0) || (numbe % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
