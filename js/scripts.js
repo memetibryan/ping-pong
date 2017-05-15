b10f49a171cf6e04cfcfb5f57d11cefdcf82d054
memetibryan <memetibryan@gmail.com>
2017-05-15 16:49:50 +0300

    Updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
+/*business interface*/
+function number(number){
+  this.number=number;
+}
+
+/*user interface*/
 $(document).ready(function() {
   $("form#formInput").submit(function(event) {
     $("ul").empty();
+    event.preventDefault();
     var number = parseInt($("input#ping").val());
 
-    for (m = 1; m <= number; m++)
-      if (m % 3 === 0 && m % 5 === 0) {
-        $("ul").append("<li>pingpong</li>");
+    for (index = 1; index <= number; index++)
+      if (index % 3 === 0 && index % 5 === 0) {
+        $("ul#output").append("<li>pingpong</li>");
 
-      } else if (m % 3 === 0) {
-      $("ul").append("<li>ping</li>");
+      } else if (index % 3 === 0) {
+      $("ul#output").append("<li>ping</li>");
 
-    } else if (m % 5 === 0) {
-      $("ul").append("<li>pong</li>");
+    } else if (index % 5 === 0) {
+      $("ul#output").append("<li>pong</li>");
 
     } else {
-      $("ul").append("<li>" + m + "</li>");
+      $("ul#output").append("<li>" + index + "</li>");
     }
-    event.preventDefault();
   });
 });
